034838312a7188d5fa6f14e67ae73e00d0d6bc9b
iannleles <iann.marcos97@gmail.com>
2022-06-23 22:52:56 -0300

    criando entidades

---
+++ AulaUmTumaH.Domain/Entities/Materia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaUmTumaH.Domain.Entities
+{
+    public class Materia : EntityBase
+    {
+        public string Nome { get; set; }
+        
+    }
+}
---
+++ AulaUmTumaH.Domain/Entities/Periodo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaUmTumaH.Domain.Entities
+{
+    public class Periodo : EntityBase
+    {        
+
+        public DateTime DataInicio { get; set; }
+
+        public DateTime DataFim { get; set; }
+
+    }
+}
