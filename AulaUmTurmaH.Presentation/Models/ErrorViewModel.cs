60a28cf458cd1f0a9873ac2f5feacc60688725e8
Wemerson Santos <wemersonmcs@outlook.com>
2022-08-04 08:52:45 -0300

    Projeto frontend

---
+++ AulaUmTurmaH.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AulaUmTurmaH.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
